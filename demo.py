b03c02a59ef1a5442967e01e6d0550f14bacdf62
protonalialol <79418621+protonalialol@users.noreply.github.com>
2023-03-31 22:34:35 -0300

    helpers update

--- demo.py
+++ demo.py
@@ -6,18 +6,20 @@ from highrisehelpers import Helpers
 
 
 class Bot(BaseBot):
+    def __init__(self):
+        self.helpers = Helpers()
     async def on_start(self, session_metadata: highrise.SessionMetadata) -> None:
         await self.highrise.chat(message=f'Demo Bot started, have fun :)')
         await self.highrise.chat(message=f'Send me some stuff UwU')
 
     async def on_chat(self, user: User, message: str):
-        print(f'{Helpers.now_timestamp()} | {user.username} [{user.id}]: "{message}"')
+        print(f'{self.helpers.now_timestamp()} | {user.username} [{user.id}]: "{message}"')
         a = await self.highrise.get_room_users()
         print(a)
         await self.highrise.chat(message=f'You said {message}')
 
     async def on_whisper(self, user: User, message: str):
-        print(f'{Helpers.now_timestamp()} | {user.username} [{user.id}] [whisper]: "{message}"')
+        print(f'{self.helpers.now_timestamp()} | {user.username} [{user.id}] [whisper]: "{message}"')
 
         match message.lower():
             case 'kill':
